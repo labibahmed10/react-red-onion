d348fc793dc59b129793f1276c4bda3c44040ff8
labibahmed10 <labib.ahmed.372@gmail.com>
2022-04-15 00:21:42 +0600

    navbar done

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BsCartPlus } from "react-icons/bs";
+
+import logo from "../../images/logo2.png";
+
+const Navbar = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex justify-between items-center px-16 py-5 bg-[aliceblue]">
+      <div>
+        <img onClick={() => navigate("/")} className="h-9" src={logo} alt="" />
+      </div>
+      <div className="flex items-center text-md space-x-8 ">
+        <Link className="text-2xl" to="/cart">
+          <BsCartPlus></BsCartPlus>
+        </Link>
+        <Link to="/login">LogIn</Link>
+        <Link className="bg-[#F91944] px-5 py-1 text-white rounded-full" to="/signup">
+          Sign Up
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
