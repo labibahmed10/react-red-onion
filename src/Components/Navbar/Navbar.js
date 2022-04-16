2d26f8ce12ae4f68cc2feba84a91c3ad990c133d
labibahmed10 <labib.ahmed.372@gmail.com>
2022-04-16 19:54:48 +0600

    sign out implemented

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,9 +3,18 @@ import { Link, useNavigate } from "react-router-dom";
 import { BsCartPlus } from "react-icons/bs";
 
 import logo from "../../images/logo2.png";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import { signOut } from "firebase/auth";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const [user] = useAuthState(auth);
+
+  const logout = () => {
+    signOut(auth);
+  };
+  console.log(user);
 
   return (
     <div className="flex justify-between items-center px-16 py-5 bg-[aliceblue]">
@@ -16,7 +25,13 @@ const Navbar = () => {
         <Link className="text-2xl" to="/cart">
           <BsCartPlus></BsCartPlus>
         </Link>
-        <Link to="/login">LogIn</Link>
+        {user ? (
+          <button onClick={logout} className="">
+            Sign Out
+          </button>
+        ) : (
+          <Link to="/login">LogIn</Link>
+        )}
         <Link className="bg-[#F91944] px-5 py-1 text-white rounded-full" to="/signup">
           Sign Up
         </Link>
